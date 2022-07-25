359595c549076adddec2bc4bfdc4c9ef626290b2
MauricioInyelco <m.inyelco.a@gmail.com>
2022-07-25 09:22:11 -0400

    primer proyecto test

---
+++ src/test/java/page/Google.java
@@ -0,0 +1,29 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+
+public class Google {
+
+    @FindBy(className = "gLFyf gsfi")
+    WebElement inputBuscador;
+
+    @FindBy(xpath = "//input[contains(@data-ved, '0ahUKEwiV6oSQoo35AhXdR7gEHVe8AfMQ4dUDCA0')]")
+    WebElement btnBuscar;
+
+    public Google(WebDriver webDriver){
+        PageFactory.initElements(webDriver, this);
+    }
+
+    public void ingresarBusqueda(String busqueda){
+        inputBuscador.sendKeys(busqueda);
+    }
+
+    public void clickBtnBuscar(){
+        btnBuscar.click();
+    }
+}
