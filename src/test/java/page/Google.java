84aeee079f339a5aae0947175846f50773620c6b
MauricioInyelco <m.inyelco.a@gmail.com>
2022-08-29 12:34:03 -0400

    clase google reemplazada

--- src/test/java/page/Google.java
+++ src/test/java/page/Google.java
@@ -9,21 +9,28 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Google {
 
-    @FindBy(className = "gLFyf gsfi")
-    WebElement inputBuscador;
+    //@FindBy(className = "gLFyf gsfi")
+    //WebElement inputBuscar;
 
-    @FindBy(xpath = "//input[contains(@data-ved, '0ahUKEwiV6oSQoo35AhXdR7gEHVe8AfMQ4dUDCA0')]")
+    // @FindBy(xpath = "//input[contains(@data-ved, '0ahUKEwiN5aujgeH5AhXgg5UCHaJSCbYQ4dUDCAs') ]")
+    // WebElement btnBuscar;
+    //@FindBy(className = "gLFyf gsfi")
+    @FindBy(xpath = "//input[contains(@class,'gLFyf gsfi')]")
+    WebElement inputBuscar;
+
+    @FindBy(xpath = "//input[contains(@value,'Buscar con Google')]")
     WebElement btnBuscar;
+
+
 
     public Google(WebDriver webDriver){
         PageFactory.initElements(webDriver, this);
     }
-
     public void ingresarBusqueda(String busqueda){
-        inputBuscador.sendKeys(busqueda);
+        inputBuscar.sendKeys(busqueda);
     }
-
     public void clickBtnBuscar(){
         btnBuscar.click();
     }
+
 }
